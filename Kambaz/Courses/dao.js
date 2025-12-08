451fd5ee2149faec5edf4c2283d0ed803a4b37a8
1kh4ng <khangp2004nj@gmail.com>
2025-12-08 17:42:36 -0500

    6.4.2 problems

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -3,12 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function CoursesDao(db) {
   function findAllCourses() {
-    return model.find();
+    return model.find({}, { name: 1, description: 1 });
   }
 
   async function findCoursesForEnrolledUser(userId) {
     const { enrollments } = db;
-    const courses = await model.find();
+    const courses = await model.find({}, { name: 1, description: 1 });
     const enrolledCourses = courses.filter((course) =>
       enrollments.some(
         (enrollment) => enrollment.user === userId && enrollment.course === course._id
